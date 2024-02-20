9fe57680b79a174ae684ff5e5d272a65fd20b675
NickSteijvers <144125086+NickSteijvers@users.noreply.github.com>
2024-02-20 12:31:55 +0100

    Added inventory and fixed issues
    i added the inventory system and fixed the first click issue and the out of bounds issue.

--- Js/main.js
+++ Js/main.js
@@ -4,15 +4,16 @@ document.getElementById("mainTitle").innerText = "Point and Click adventure game
 const gameWindow = document.getElementById("gameWindow");
 
 //inventory
+let inventory = [];
 const inventorylist = document.getElementById("inventorylist")
 
 //Main Character
 const mainCharacter = document.getElementById("hero");
 const offsetcharacter = 16;
 
-const tree1 = document.getElementById("squareTree");
-const door1 = document.getElementById("door");
+const door1 = document.getElementById("rustyKeyDoor");
 const key1 = document.getElementById("key");
+
 
 gameWindow.onclick = function (e) 
 {
@@ -20,31 +21,107 @@ gameWindow.onclick = function (e)
     var x = e.clientX - rect.left;
     var y = e.clientY - rect.top;
 
-    //FIX: character doen't animate on first click
-    //FIX: Character can go out of bounds when clicking on itself
     //TODO: calc offset based on character size
-    mainCharacter.style.left = x - offsetcharacter + "px";
-    mainCharacter.style.top = y - offsetcharacter + "px";
+
+    if(e.target.id !== "heroImage")
+    {
+     mainCharacter.style.left = x - offsetcharacter + "px";
+     mainCharacter.style.top = y - offsetcharacter + "px";   
+    }
+    
     switch (e.target.id)
     {
-        case "squareTree" :
-            tree1.style.opacity = 0.5;
-        break;
-
         case "key" :
-            console.log("You have found the key!!!")
-            document.getElementById("key").remove();
-            const keyelement = document.createElement("li");
-            keyelement.id = "inv-key";
-            keyelement.innerText = "key";
-            inventorylist.appendChild(keyelement)
+           getItem("Rusty key", "rustyKey");
             break;
-        case "door" :
-            door1.style.opacity = 0.5;
+        case "rustyKeyDoor" :
+            if(checkItem("Rusty key"))
+            {
+                door1.style.opacity = 0.5; 
+                console.log("I opened the door. Yeah!");
+            }
+            else if (checkItem("Coin"))
+            {
+                console.log("O no I lost my coin and it didnt open the door... I feel kinda stupid");
+                removeItem("Coin", "coin");
+            }
+
+            else 
+            {
+                console.log("Fuck the door is locked and i dont have the rusty key")
+            }
             break;
+
+        case "well" :
+            getItem("Coin", "coin");
+             break;
 
         default:
-            tree1.style.opacity = 1;
             door1.style.opacity = 1;
+        break;
+    }
+
+    /**
+     * Checks if the value exists within the array
+     * If not then it adds value to the array and use showItem function
+     * @param {string} itemName 
+     * @param {string} itemId 
+     */
+    //GetItem
+    function getItem(itemName, itemId)
+    {
+        if (!checkItem(itemName))
+        {
+            inventory.push(itemName);
+            showItem(itemName, itemId);
+        }
+        console.log (inventory);
+
+        //Removing visible items:
+        //document.getElementById("key").remove();
+
+    }
+    //UseItem
+    function useItem (itemName, itemId)
+    {
+
+    }
+    //CheckItem
+    function checkItem(itemName)
+    {
+        return inventory.includes(itemName);
+
+    }
+
+    //ShowItem
+    /**
+     * Needs a name for displaying item and a html id name
+     * @param {string} itemName 
+     * @param {string} itemId 
+     */
+    function showItem (itemName, itemId)
+    {
+        console.log('You\'ve found a ' + itemName + '!')
+        const keyelement = document.createElement("li");
+        keyelement.id = itemId;
+        keyelement.innerText = itemName;
+        inventorylist.appendChild(keyelement)
+
+    }
+
+    /**
+     * Removes item from array and the element within the HTML
+     * @param {string} itemName 
+     * @param {string} itemId 
+     */
+    //RemoveItem
+    function removeItem (itemName, itemId)
+    {
+        //remove item in array
+       inventory = inventory.filter(function(newInventory){
+        return newInventory !== itemName;
+       });
+       document.getElementById(itemId).remove();
+
     }
 }
