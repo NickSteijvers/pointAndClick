b9b382d77cc879c2fd7735c6945f6773120a4b49
NickSteijvers <144125086+NickSteijvers@users.noreply.github.com>
2024-02-23 12:54:19 +0100

    Added save file reworked the inventory system and added gamestate
    Reworked inventory for easier use
    Added a way to fetch saved files
    Added the gameState for easier checks of certain things in game

--- Js/main.js
+++ Js/main.js
@@ -1,95 +1,126 @@
-document.getElementById("mainTitle").innerText = "Point and Click adventure game"; 
+document.getElementById("mainTitle").innerText = "Point and Click adventure game";
 
-//Game window refrence
-const gameWindow = document.getElementById("gameWindow");
-
-//inventory
-let inventory = [];
-const inventorylist = document.getElementById("inventorylist")
-
-//Main Character
-const mainCharacter = document.getElementById("hero");
-const offsetcharacter = 16;
-
-const door1 = document.getElementById("rustyKeyDoor");
-const key1 = document.getElementById("key");
-
-
-gameWindow.onclick = function (e) 
+//Game State
+let gameState =
 {
-    var rect = gameWindow.getBoundingClientRect();
-    var x = e.clientX - rect.left;
-    var y = e.clientY - rect.top;
+    "inventory": [],
+    "coinPickedUp": false
+}
 
-    //TODO: calc offset based on character size
-
-    if(e.target.id !== "heroImage")
-    {
-     mainCharacter.style.left = x - offsetcharacter + "px";
-     mainCharacter.style.top = y - offsetcharacter + "px";   
+//load data from save file
+/*fetch('data/save.json').then((response) => {
+    if (response.status == 404) {
+        alert('file not found!');
+    } else {
+        return response.json();
     }
-    
-    switch (e.target.id)
-    {
-        case "key" :
-           getItem("Rusty key", "rustyKey");
-            break;
-        case "rustyKeyDoor" :
-            if(checkItem("Rusty key"))
-            {
-                door1.style.opacity = 0.5; 
-                console.log("I opened the door. Yeah!");
-            }
-            else if (checkItem("Coin"))
-            {
-                console.log("O no I lost my coin and it didnt open the door... I feel kinda stupid");
-                removeItem("Coin", "coin");
-            }
+}).then((resJson) => {
+    gameState = resJson;
+    runGame();
+}).catch((error) => {
+    console.error(error)
+})*/
 
-            else 
-            {
-                console.log("Fuck the door is locked and i dont have the rusty key")
-            }
-            break;
+//Comment out runGame(); if you want to use the save file function
+runGame();
+function runGame() {
+    debugger;
 
-        case "well" :
-            getItem("Coin", "coin");
-             break;
-        
-        case "statue" :
-            console.log("Hey you the key is on the platform on the roof of the building")
 
-        default:
-            door1.style.opacity = 1;
-        break;
+    //Game window refrence
+    const gameWindow = document.getElementById("gameWindow");
+
+    //inventory
+    const inventorylist = document.getElementById("inventorylist")
+
+    //Main Character
+    const mainCharacter = document.getElementById("hero");
+    const offsetcharacter = 16;
+
+    const door1 = document.getElementById("rustyKeyDoor");
+    const key1 = document.getElementById("key");
+
+
+    gameWindow.onclick = function (e) {
+        var rect = gameWindow.getBoundingClientRect();
+        var x = e.clientX - rect.left;
+        var y = e.clientY - rect.top;
+
+        //TODO: calc offset based on character size
+        //TODO: add dialogue
+
+        if (e.target.id !== "heroImage") {
+            mainCharacter.style.left = x - offsetcharacter + "px";
+            mainCharacter.style.top = y - offsetcharacter + "px";
+        }
+
+        switch (e.target.id) {
+            case "key":
+                console.log("pickup key");
+                document.getElementById("key").remove();
+                changeInventory('key', "add");
+                break;
+            case "rustyKeyDoor":
+                if (checkItem("key")) {
+                    door1.style.opacity = 0.5;
+                    console.log("I opened the door. Yeah!");
+                }
+                else if (checkItem("coin")) {
+                    changeInventory("coin", "remove")
+                    console.log("O no I lost my coin and it didnt open the door... I feel kinda stupid");
+                }
+
+                else {
+                    console.log("Fuck the door is locked and i dont have the key")
+                }
+                break;
+
+            case "well":
+                if (gameState.coinPickedUp == false) {
+                    console.log("pickup coin");
+                    changeInventory('coin', "add");
+                    gameState.coinPickedUp = true;
+                }
+                else {
+                    console.log("There are no more coins in this well!")
+                }
+                break;
+
+            case "statue":
+                console.log("Hey you the key is on the platform on the roof of the building")
+
+            default:
+                door1.style.opacity = 1;
+                break;
+        }
     }
 
     /**
-     * Checks if the value exists within the array
-     * If not then it adds value to the array and use showItem function
+     * Add or remove item in inventory
      * @param {string} itemName 
-     * @param {string} itemId 
+     * @param {string} action 
      */
-    //GetItem
-    function getItem(itemName, itemId)
-    {
-        if (!checkItem(itemName))
-        {
-            inventory.push(itemName);
-            showItem(itemName, itemId);
+    function changeInventory(itemName, action) {
+        if (itemName == null || action == null) {
+            console.error("Wrong parameters given to changeInventory");
+            return;
         }
-        console.log (inventory);
 
-        //Removing visible items:
-        //document.getElementById("key").remove();
+        switch (action) {
+            case 'add':
+                gameState.inventory.push(itemName);
+                break;
+            case 'remove':
+                gameState.inventory = gameState.inventory.filter(function (newInventory) {
+                    return newInventory !== itemName;
+                });
+                document.getElementById("inv-" + itemName).remove();
+                break;
+        }
+        updateInventory(gameState.inventory, inventorylist)
     }
-
-    //UseItem
-    function useItem (itemName, itemId)
-    {
-
-    }
-
+    //Removing visible items:
+    //document.getElementById("key").remove();
 
     //CheckItem
     /**
@@ -97,40 +128,18 @@ gameWindow.onclick = function (e)
      * @param {string} itemName 
      * @returns 
      */
-    function checkItem(itemName)
-    {
-        return inventory.includes(itemName);
+    function checkItem(itemName) {
+        return gameState.inventory.includes(itemName);
 
     }
 
-    //ShowItem
-    /**
-     * Needs a name for displaying item and a html id name
-     * @param {string} itemName 
-     * @param {string} itemId 
-     */
-    function showItem (itemName, itemId)
-    {
-        console.log('You\'ve found a ' + itemName + '!')
-        const keyelement = document.createElement("li");
-        keyelement.id = itemId;
-        keyelement.innerText = itemName;
-        inventorylist.appendChild(keyelement)
-    }
-
-      //RemoveItem
-    /**
-     * Removes item from array and the element within the HTML
-     * @param {string} itemName 
-     * @param {string} itemId 
-     */
-    function removeItem (itemName, itemId)
-    {
-        //remove item in array
-       inventory = inventory.filter(function(newInventory){
-        return newInventory !== itemName;
-       });
-       document.getElementById(itemId).remove();
-
+    function updateInventory(inventory, inventorylist) {
+        inventorylist.innerHTML = '';
+        inventory.forEach(function (item) {
+            const inventoryItem = document.createElement("li");
+            inventoryItem.id = 'inv-' + item;
+            inventoryItem.innerText = item;
+            inventorylist.appendChild(inventoryItem)
+        })
     }
 }
