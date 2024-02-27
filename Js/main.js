2cda04688a08042ef419e91d62e1de70f53f53b7
NickSteijvers <144125086+NickSteijvers@users.noreply.github.com>
2024-02-27 13:06:28 +0100

    FIxed shit

--- Js/main.js
+++ Js/main.js
@@ -40,11 +40,15 @@ function runGame() {
     const heroSpeech = document.getElementById("heroSpeech");
     const counterSpeech = document.getElementById("counterSpeech");
 
+    //audio for dialogue
+    const heroAudio = document.getElementById("heroAudio");
+    const counterAudio = document.getElementById("counterAudio");
 
+    //avatar
+    const counterAvatar = document.getElementById("counterAvatar");
 
     const door1 = document.getElementById("rustyKeyDoor");
     const key1 = document.getElementById("key");
-
 
     gameWindow.onclick = function (e) {
         var rect = gameWindow.getBoundingClientRect();
@@ -54,11 +58,12 @@ function runGame() {
         //TODO: calc offset based on character size
         //TODO: add dialogue
 
-        if (e.target.id !== "heroImage") {
-            mainCharacter.style.left = x - offsetcharacter + "px";
-            mainCharacter.style.top = y - offsetcharacter + "px";
-        }
-
+        if(counterSpeech.style.opacity == 0 && heroSpeech.style.opacity == 0)
+        {
+            if (e.target.id !== "heroImage" ) {
+                mainCharacter.style.left = x - offsetcharacter + "px";
+                mainCharacter.style.top = y - offsetcharacter + "px";
+            }
         switch (e.target.id) {
             case "key":
                 console.log("pickup key");
@@ -68,36 +73,48 @@ function runGame() {
             case "rustyKeyDoor":
                 if (checkItem("key")) {
                     door1.style.opacity = 0.5;
-                    showMessage(heroSpeech, "I opened the door. Yeah!")
+                    showMessage(heroSpeech, "I opened the door. Yeah!", heroAudio)
                 }
                 else if (checkItem("coin")) {
                     changeInventory("coin", "remove")
-                    showMessage(heroSpeech, "O no I lost my coin and it didnt open the door... I feel kinda stupid")
+                    showMessage(heroSpeech, "O no I lost my coin and it didnt open the door... I feel kinda stupid", heroAudio)
                 }
 
                 else {
-                    showMessage(heroSpeech, "Fuck the door is locked and i dont have the key")
+                    showMessage(heroSpeech, "Fuck the door is locked and i dont have the key", heroAudio);
                 }
                 break;
 
             case "well":
                 if (gameState.coinPickedUp == false) {
-                    showMessage(heroSpeech, "I found a coin :)")
+                    showMessage(heroSpeech, "I found a coin :)", heroAudio)
                     console.log("pickup coin");
                     changeInventory('coin', "add");
                     gameState.coinPickedUp = true;
                 }
                 else {
-                    console.log("There are no more coins in this well!")
+                    showMessage(heroSpeech, "There are no more coins in this well!", heroAudio)
                 }
                 break;
 
             case "statue":
-                showMessage(counterSpeech, "Hey you the key is on the platform on the roof of the building")
+                showMessage(heroSpeech, "Hey a statue... Looks nice", heroAudio);
+                setTimeout(function(){counterAvatar.style.opacity = 1;}, 4 * sec);
+                setTimeout(showMessage, 4 * sec, counterSpeech, "I can talk you know...", counterAudio);
+                setTimeout(showMessage, 8 * sec, heroSpeech, "HOLY SHIT!!! That scared the shit out of me",heroAudio);
+                setTimeout(showMessage, 12 * sec, counterSpeech, "Yeah Yeah i get it, I scared you",counterAudio);
+                setTimeout(showMessage, 16 * sec, heroSpeech, "Okay so i have a question for you",heroAudio)
+                setTimeout(showMessage, 20 * sec, counterSpeech, "Yeah Yeah I know I heard you want the key for the door",counterAudio);
+                setTimeout(showMessage, 24 * sec, heroSpeech, "How did you........",heroAudio);
+                showMessage(showMessage, 28 * sec, counterSpeech, "The key is on the platform on the roof of the building",counterAudio);
+                showMessage(showMessage, 32 * sec, heroSpeech, "Thanks!!!", heroAudio)
+                setTimeout(function(){counterAvatar.style.opacity = 0;} , 36 * sec);
+                
 
             default:
                 door1.style.opacity = 1;
                 break;
+        }
         }
     }
 
@@ -146,24 +163,36 @@ function runGame() {
             inventoryItem.id = 'inv-' + item;
             inventoryItem.innerText = item;
             inventorylist.appendChild(inventoryItem)
-        })
+        });
     }
 
     /**
-     * It will show dialogue
+     * It will show dialogue and trigger sound
      * @param {getElementById} targetBubble 
      * @param {string} message 
+     * @param {getElementById} targetSound
      */
-    function showMessage(targetBubble, message)
+    function showMessage(targetBubble, message, targetSound)
     {
+        targetSound.currentTime = 0;
+        targetSound.play();
         targetBubble.innerText = message;
         targetBubble.style.opacity = 1;
-        setTimeout(hideMessage, 4 * sec, targetBubble);
+
+        setTimeout(hideMessage, 4 * sec, targetBubble, targetSound);
     }
-    function hideMessage(targetBubble)
+
+    /**
+     * hides message and pauses the audio
+     * @param {getElementById} targetBubble 
+     * @param {getElementById} targetSound 
+     */
+    function hideMessage(targetBubble, targetSound)
     {
+        targetSound.pause();
         targetBubble.innerText = "...";
         targetBubble.style.opacity = 0;
+        checkDialog = false;
     }
 
 }
